840261b43a701f9a02cf8c68e734438652d0b35b
albanybuipe96 <mohammedarahman96@gmail.com>
2021-01-26 01:14:27 -0800

    auth service completed

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ticketing-mcs
-# ticketing-mcs
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
+import { BadRequestError } from '../errors/bad-request-error'
 import { validateRequest } from '../middlewares/validate-request'
+import { User } from '../models/user'
+import { Password } from '../services/password'
+import jwt from 'jsonwebtoken'
 
 const router = express.Router()
 
@@ -14,11 +18,35 @@ router.post('/api/users/signin', [
         .withMessage('Password not valid')
 ],
 validateRequest,
-(req: Request, res: Response) => {
-    
+async (req: Request, res: Response) => {
+    const { email, password } = req.body
+
+    const existingUser = await User.findOne({ email })
+    if (!existingUser) {
+        throw new BadRequestError('Invalid Credentials')
+    }
+
+    const passwordsMatch = await Password.compare(existingUser.password, password)
+
+    if (!passwordsMatch) {
+        throw new BadRequestError('Invalid Credentials')
+    }
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: existingUser.id,
+        email: existingUser.email
+    }, process.env.JWT_KEY!)
+
+    // Store JWT on session obj
+    req.session = {
+        jwt: userJwt
+    }
+
+    res.status(200).send(existingUser)
     
 })
 
 export { router as signinRouter }
 
-// TODO: Section 9.19
+// TODO: Section 9.21
